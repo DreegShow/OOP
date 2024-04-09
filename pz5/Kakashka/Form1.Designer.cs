3406912b6031452797d7483237ae2b4b1fced299
DreegShow <132409816+DreegShow@users.noreply.github.com>
2024-04-09 15:01:25 +0300

    Add files via upload

---
+++ pz5/Kakashka/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Kakashka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(50, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(325, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Назва";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(305, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Капітан прайс";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
+            this.button3.Location = new System.Drawing.Point(50, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(585, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Замовити";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(680, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ pz5/Kakashka/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kakashka
+{
+    public partial class Form1 : Form
+    {
+        private List<MenuItem> menuItems;
+        private int currentItemIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMenuItems();
+            ShowCurMenuItem();
+        }
+
+/*        private void InitializeMenuItems()
+        {
+            if (menuItems == null) {  menuItems = new List<MenuItem>(); }
+            menuItems = new List<MenuItem>
+            {
+                new MenuItem("12312", 123, Properties.Resources.Koza),
+            };
+        }*/
+
+        private void InitializeMenuItems()
+        {
+            menuItems = new List<MenuItem>
+            {
+                new MenuItem("Смажена коза", 105, Properties.Resources.Koza),
+                new MenuItem("якась фігня", 24, Properties.Resources.SCI),
+                new MenuItem("квадрат Малєвіча", 9999, Properties.Resources.WallPaper)
+            };
+        }
+
+        private void ShowCurMenuItem()
+        {
+            MenuItem currentItem = menuItems[currentItemIndex];
+            label1.Text = currentItem.Name;
+            label2.Text = $"цена: {currentItem.Price} грн";
+            pictureBox1.Image = currentItem.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentItemIndex = (currentItemIndex - 1 + menuItems.Count) % menuItems.Count;
+            ShowCurMenuItem();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentItemIndex = (currentItemIndex + 1) % menuItems.Count;
+            ShowCurMenuItem();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            //MessageBox.Show("1211496");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "ресторант";
+        }
+
+    }
+
+    public class MenuItem
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public Image Image { get; }
+
+
+        public MenuItem(string name, decimal price, Image image)
+        {
+            Name = name;
+            Price = price;
+            Image = image;
+        }
+    }
+}
